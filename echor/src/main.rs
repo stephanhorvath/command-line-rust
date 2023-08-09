fa8a30dba008ccf9156b0b81c776975f50cbb187
Stephan Horvath <stephanhm@pm.me>
2023-08-09 20:10:22 +0200

    Refactor Using Clap Crate
    Refactor reading command-line arguments using Clap, to generate better
    documentation and error messages.
    - also modified .gitignore to correclty ignore target/ directory

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,5 +1,24 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    println!("{:?}", env::args());
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Stephan Horvath <stephanhm@pm.me>")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    println!("{:#?}", matches);
 }
