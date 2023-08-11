202fff659c0f5fffcb2abb8173e1385ee66609a4
Stephan Horvath <stephanhm@pm.me>
2023-08-11 10:35:26 +0200

    Validate Flag Values - My Version
    The validation of reading the values passed for -n and -c in this commit
    is my code before the solution in the book.

---
+++ headr/src/lib.rs
@@ -0,0 +1,98 @@
+use clap::{App, Arg, ArgGroup};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Stephan Horvath <stephanhm@pm.me>")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .conflicts_with("lines")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: if matches.is_present("lines") {
+            match parse_positive_int(matches.value_of("lines").unwrap()) {
+                Ok(n) => n,
+                Err(e) => panic!("illegal line count -- {}", e),
+            }
+        } else {
+            10
+        },
+        bytes: if !matches.is_present("bytes") {
+            None
+        } else {
+            match parse_positive_int(
+                matches
+                    .value_of("bytes")
+                    .expect("Something went really wrong"),
+            ) {
+                Ok(v) => Some(v),
+                Err(v) => panic!("illegal byte count -- {}", v),
+            }
+        },
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3 is an OK integer
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // any string is an error
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // a zero is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
